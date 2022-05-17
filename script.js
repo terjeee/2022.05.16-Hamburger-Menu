f39a8969f42cfc4ff38353c0390829cf69d86190
terjeee <96795863+terjeee@users.noreply.github.com>
2022-05-17 07:42:44 +0200

    ferdig (improvisert)

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const navOpen = document.querySelector(`#navBarOpen`);
 const navClose = document.querySelector(`#navBarClose`);
+const containerOverlay = document.querySelector(`.containerOverlay`);
 
-navOpen;
+navOpen.addEventListener(`click`, function (e) {
+  navOpen.classList.add(`hidden`);
+  containerOverlay.classList.remove(`hidden`);
+});
+
+navClose.addEventListener(`click`, function (e) {
+  navOpen.classList.remove(`hidden`);
+  containerOverlay.classList.add(`hidden`);
+});
